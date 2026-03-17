3f3102cbf734772841abfd2cba92d7cc99aa35f5
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-03-17 22:47:05 +0530

    data processing file

--- 01_RNN/Rough works/01_data_processing.py
+++ 01_RNN/Rough works/01_data_processing.py
@@ -6,3 +6,9 @@ t.set_default_device(DEVICE)
 
 print(t.get_default_device())
 # %%
+import string
+import unicodedata
+
+allowed_char = string.ascii_letters + ".,;'" + "_"
+print(allowed_char)
+# %%
